9331f9fc679f57033cc80ece7cbd1f19fcc4431f
neeraj <win7@192.168.1.8>
2016-10-05 19:34:28 +0530

    Initial checkin for salestool

---
+++ SalesTool/src/com/sales/Main.java
@@ -0,0 +1,20 @@
+package com.sales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData sd=new SalesData();
+		
+		mainDisplay();
+		sd.display();
+
+	}
+	
+	private static void mainDisplay()
+	{
+		System.out.println("mainDisplay Method of main class");
+	}
+
+}
---
+++ SalesTool/src/com/sales/SalesData.java
@@ -0,0 +1,9 @@
+package com.sales;
+
+public class SalesData {
+
+	public void display()
+	{
+		System.out.println("Display Method Of SalesData class");
+	}
+}
